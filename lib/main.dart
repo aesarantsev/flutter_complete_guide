2a6979e76959cc1da8ccd20135f6fd0266fbaefe
Andrey Sarantsev <andrrr96@yandex.ru>
2022-02-06 18:27:01 +1000

    Lesson 031

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,36 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestions() {
+    print('ANswer question!');
+  }
+
+  var questions = [
+    'What\'s your favorite color?',
+    'What\'s your favorite animal?',
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(title: Text('My first App')),
-      body: Text('My first App body'),
+      body: Column(
+        children: [
+          Text('The question!'),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: answerQuestions,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: answerQuestions,
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: answerQuestions,
+          ),
+        ],
+      ),
     ));
   }
 }
